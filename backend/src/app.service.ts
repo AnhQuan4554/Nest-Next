6bb1f0b1dd1ed3bafb6f24f13cea81edf96121a6
Quân <quanmanchester0405@gmail.com>
2023-07-12 14:32:21 +0700

    server and client

---
+++ backend/src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!gfyuisdgfhidsuahyrtfiu ';
+  }
+  testRenderString(): number {
+    return 21312;
+  }
+}
